c1f7ea6a847e2d9408d740e022f09b69f55c61ed
Vamsi Madhav <vmcse09@gmail.com>
2023-11-13 12:22:58 +0530

    Completed exercise 3.2

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><br>${new Date()}`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
