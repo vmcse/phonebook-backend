86883cbcc156384e23e206179bc325e9ee13a175
Vamsi Madhav <vmcse09@gmail.com>
2023-11-14 19:52:06 +0530

    Completed exercise 3.8

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express();
 
 const morgan = require("morgan");
 
+morgan.token("data", (request) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  } else {
+    return "";
+  }
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
